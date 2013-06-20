2594de21ed50e7e6b070ed049c7088e9c8c2c5f3
Henrique Magalhães <henrique.albertino@gmail.com>
2013-06-19 23:32:06 -0300

    Added 'Set alarm' feature. Needs some refactoring.

--- src/main/java/org/prgrmmr/quesono/SugestoesActivity.java
+++ src/main/java/org/prgrmmr/quesono/SugestoesActivity.java
@@ -1,8 +1,19 @@
 package org.prgrmmr.quesono;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SugestoesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
@@ -18,9 +29,23 @@ public class SugestoesActivity extends Activity {
         }
 
         String[] sugestoes = getIntent().getStringArrayExtra(MainActivity.SUGESTOES);
-        ((TextView) findViewById(R.id.txtSugestoes1)).setText(sugestoes[3]);
-        ((TextView) findViewById(R.id.txtSugestoes2)).setText(sugestoes[2]);
-        ((TextView) findViewById(R.id.txtSugestoes3)).setText(sugestoes[1]);
-        ((TextView) findViewById(R.id.txtSugestoes4)).setText(sugestoes[0]);
+        ((Button) findViewById(R.id.btnDespertador1)).setText(sugestoes[3]);
+        ((Button) findViewById(R.id.btnDespertador2)).setText(sugestoes[2]);
+        ((Button) findViewById(R.id.btnDespertador3)).setText(sugestoes[1]);
+        ((Button) findViewById(R.id.btnDespertador4)).setText(sugestoes[0]);
+    }
+
+    public void setarDespertador(View v) {
+        Intent novoAlarme = new Intent(AlarmClock.ACTION_SET_ALARM);
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new SimpleDateFormat("HH:mm").parse(((Button) v).getText().toString()));
+            novoAlarme.putExtra(AlarmClock.EXTRA_HOUR, cal.get(Calendar.HOUR_OF_DAY));
+            novoAlarme.putExtra(AlarmClock.EXTRA_MINUTES, cal.get(Calendar.MINUTE));
+            startActivity(novoAlarme);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Ops, não deu pra arrumar seu despertador. =/", 5).show();
+        }
     }
 }
