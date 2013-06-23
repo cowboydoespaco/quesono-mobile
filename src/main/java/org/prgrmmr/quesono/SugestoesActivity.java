d42af9d94ca3aa1e5464099e2e86aa97c595acae
Henrique Magalhães <henrique.albertino@gmail.com>
2013-06-23 12:10:16 -0300

    'Fixed' the styling, added dialog confirmation;
    Setting alarms won't be so intrusive now.
    The action bar still needs work.

--- src/main/java/org/prgrmmr/quesono/SugestoesActivity.java
+++ src/main/java/org/prgrmmr/quesono/SugestoesActivity.java
@@ -1,10 +1,11 @@
 package org.prgrmmr.quesono;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.AlarmClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,7 +14,6 @@ import android.widget.Toast;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class SugestoesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +35,25 @@ public class SugestoesActivity extends Activity {
         ((Button) findViewById(R.id.btnDespertador4)).setText(sugestoes[0]);
     }
 
+    public void confirmacaoDespertador(final View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirmacaoDespertador).setTitle(R.string.tituloConfirmacaoDespertador);
+        builder.setPositiveButton(R.string.sim, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                setarDespertador(v);
+            }
+        });
+
+        builder.setNegativeButton(R.string.nao, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // nada?
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     public void setarDespertador(View v) {
         Intent novoAlarme = new Intent(AlarmClock.ACTION_SET_ALARM);
         try {
@@ -42,6 +61,7 @@ public class SugestoesActivity extends Activity {
             cal.setTime(new SimpleDateFormat("HH:mm").parse(((Button) v).getText().toString()));
             novoAlarme.putExtra(AlarmClock.EXTRA_HOUR, cal.get(Calendar.HOUR_OF_DAY));
             novoAlarme.putExtra(AlarmClock.EXTRA_MINUTES, cal.get(Calendar.MINUTE));
+            novoAlarme.putExtra(AlarmClock.EXTRA_MESSAGE, "QueSono.com");
             startActivity(novoAlarme);
         } catch (ParseException e) {
             e.printStackTrace();
